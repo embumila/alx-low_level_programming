2a847db67762e996a50e59ed3cb089da57c7d6ba
Emmanuel Mbumila <mbuxcompany@gmail.com>
2022-12-08 06:21:14 -0800

    Fix: Commit style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 /**
- * * main -> Entry point of the program
- * * Return: Always 0 (Success)
- * */
+ * main -> Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
