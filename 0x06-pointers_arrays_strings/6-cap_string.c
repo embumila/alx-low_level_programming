9cc8594ecafe54d86d22b25dc75c6280c3416f51
Emmanuel Mbumila <mbuxcompany@gmail.com>
2022-12-22 07:49:40 -0800

    Adds space

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,7 +10,8 @@
 char *cap_string(char *entry)
 {
 int conversion, index, count;
-char chars[] = {' ', ',', ';', '.', '!','?', '"', '(', ')', '{', '}',  '\t', '\n', '\0'};
+char chars[] = {' ', ',', ';', '.', '!',
+		 '?', '"', '(', ')', '{', '}',  '\t', '\n', '\0'};
 conversion = 32;
 for (index = 0; entry[index] != '\0'; index++)
 {
