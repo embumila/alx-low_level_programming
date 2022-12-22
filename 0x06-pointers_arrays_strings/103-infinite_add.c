5df50c53da457739ac28176d7d453c8460021420
Emmanuel Mbumila <mbuxcompany@gmail.com>
2022-12-22 08:46:42 -0800

    Fixing the file

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -12,7 +12,7 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int a_len = 0, b_len = 0, carry = 0, a, b, sum, biggest; 
+int a_len = 0, b_len = 0, carry = 0, a, b, sum, biggest;
 while (n1[a_len] != '\0')
 a_len++;
 while (n2[b_len] != '\0')
@@ -23,7 +23,7 @@ else
 biggest = b_len;
 if ((biggest + 1) >= size_r)
 return (0);
-r[biggest + 1] = '\0';  
+r[biggest + 1] = '\0';
 while (biggest >= 0)
 {
 a = (n1[a_len - 1] - '0');
