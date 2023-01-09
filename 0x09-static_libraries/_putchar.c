a66be7432859029c94f3397743dc41bfa2a500d6
Emmanuel Mbumila <mbuxcompany@gmail.com>
2023-01-09 12:04:14 -0800

    Fixing .sh file

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,5 +1,9 @@
 #include "main.h"
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <unistd.h>
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -9,5 +13,5 @@
 
 int _putchar(char c)
 {
-return (write(c));
+  return (write(1, &c, 1));
 }
