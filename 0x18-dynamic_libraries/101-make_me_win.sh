b38d0b9842b4055dcc962f8777e8df749c5882db
Emmanuel Mbumila <mbuxcompany@gmail.com>
2023-03-20 16:35:46 -0700

    add:files

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-wget -O /tmp/win.so https://github.com/Zevrov/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/win.so
-export LD_PRELOAD=/tmp/win.so
+wget  https://github.com/holbertonschool/0x18.c
